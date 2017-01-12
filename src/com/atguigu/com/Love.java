d5fa6437a4f0b4eab1972381af0c0adcc0e63a7e
waisl1418626891 <1418626891@qq.com>
2017-01-12 13:58:26 +0800

    李玉婷先提交，现在汪涛做修改提交演示冲突

--- src/com/atguigu/com/Love.java
+++ src/com/atguigu/com/Love.java
@@ -4,5 +4,6 @@ public class Love {
 	public static void main(String[] args) {
 		System.out.println("李玉婷01*************");
 		System.out.println("李玉婷01*************");
+		System.out.println("李玉婷先提交【【【【【】】】】】");
 	}
 }
