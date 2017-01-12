1d9143e3e3cbac4bbbb8ce30611375611de42ab0
liyutinglove <wt4488254@163.com>
2017-01-12 13:21:16 +0800

    李玉婷第一次修改提交

--- src/com/atguigu/com/Love.java
+++ src/com/atguigu/com/Love.java
@@ -2,7 +2,7 @@ package com.atguigu.com;
 
 public class Love {
 	public static void main(String[] args) {
-		System.out.println("李玉婷01*************");
-		System.out.println("李玉婷01*************");
+		System.out.println("李玉婷02*************");
+		System.out.println("李玉婷02*************");
 	}
 }
