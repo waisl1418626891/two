f84a28116334b431ee469d3555f1b2bedb67d7f4
waisl1418626891 <1418626891@qq.com>
2017-01-12 13:05:12 +0800

    汪涛第二次修改提交

--- src/com/atguigu/com/Love.java
+++ src/com/atguigu/com/Love.java
@@ -3,5 +3,6 @@ package com.atguigu.com;
 public class Love {
 	public static void main(String[] args) {
 		System.out.println("李玉婷01*************");
+		System.out.println("李玉婷01*************");
 	}
 }
